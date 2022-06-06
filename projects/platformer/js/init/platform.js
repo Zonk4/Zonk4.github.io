3b3183e787217856124586c1d9ee178f3727c32b
Zonk4 <kingkju2@gmail.com>
2022-06-06 20:59:03 +0000

    web

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -38,9 +38,9 @@
         createPlatform(659, 625, 0.6);// far right bottom platform 
         createPlatform(370, 525, 0.6);// middle platform 
         createPlatform(0, 460, 0.2); // far left platform 
-        createPlatform(659, 430, 0.6);// last coin plat hard
-        createPlatform()
-        // example:
+        createPlatform(659, 430, 0.6);// last coin plat hard vv
+        createPlatform(659, 430, 0.6)
+        
         createPlatform(400, 250);
         createPlatform(170, 460, 0.5);
         
